e77b680f2faad7559698c0bf56f47fcc11975717
AhmedShahan <shahan.ahmed001@gmail.com>
2025-03-19 12:16:43 +0600

    involvement is added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var typed =new Typed(".multiple-text",{
-    strings:["Machine Learning","Deep Learning", "Deep Reinforcement Learning", "Computer Vision"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 100,
-    loop:true
-})
+    var typed =new Typed(".multiple-text",{
+        strings:["Machine Learning","Artificial Neural Network (ANN)", "Computer Vision & Image Processing", "Generative AI (GenAI)", "Natural Language Processing (NLP)", "Large Language Moddel (LLM)", "RAG & Langchain Ecosysthom", "Deep Reinforcemnt Learning (DRL)", "Artificial General Intelligence (AGI)"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 100,
+        loop:true
+    })
 
 
-// var typed=new Typed("")
+    // var typed=new Typed("")
